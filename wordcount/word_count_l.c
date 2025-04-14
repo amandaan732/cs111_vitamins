6ab892dc214eef91128dcc00b0cd146047827675
amandaan732 <amandaan732@ucla.edu>
2025-04-14 04:09:51 +0000

    implemented word_count_l.c

--- wordcount/word_count_l.c
+++ wordcount/word_count_l.c
@@ -27,23 +27,59 @@
 #include "word_count.h"
 
 void init_words(word_count_list_t *wclist) {
-    /* TODO */
+    list_init(wclist);
 }
 
 size_t len_words(word_count_list_t *wclist) {
-    /* TODO */
-    return 0;
+    return list_size(wclist);
 }
 
 word_count_t *find_word(word_count_list_t *wclist, char *word) {
-    /* TODO */
+    //go through wc list and check if the list element == the word
+    //need to check the types of each thing 
+    //word_count_t is a struct --> we need to make wc's list_entry into a word_count_t typedef
+    //then that word_count_t's char *word element should be compared to *word 
+        //if they are equal rerturn the struct, otherwise return NULL
+
+    for (struct list_elem *current = list_begin(wclist); current != list_end(wclist); current = list_next(current)) {
+        word_count_t *curStruct = list_entry(current, word_count_t, elem);
+        if (strcmp(curStruct->word, word) == 0) {
+            return curStruct;
+        }
+    }
+    // char checkWord;
+    // if(strcmp(checkWord, word) == 0) {
+    //     return checkWord;
+    // }
     return NULL;
 }
 
 word_count_t *add_word_with_count(word_count_list_t *wclist, char *word,
                                   int count) {
-    /* TODO */
-    return NULL;
+    //check if the word exists, if it does increment the count and return current node
+    //if not found
+        //allocate memory for new node --> like in word_count.c
+        //wc->word = word and wc->count = count
+        //insert into wclist
+        //return wc
+
+    word_count_t *wc = find_word(wclist, word);
+    if (wc != NULL) {
+        wc->count += count;
+        return wc;
+    }
+
+    wc = malloc(sizeof(word_count_t));
+    if (wc == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    wc->word = word;
+    wc->count = count;
+
+    list_push_back(wclist, &wc->elem);
+    return wc;
 }
 
 word_count_t *add_word(word_count_list_t *wclist, char *word) {
@@ -51,13 +87,24 @@ word_count_t *add_word(word_count_list_t *wclist, char *word) {
 }
 
 void fprint_words(word_count_list_t *wclist, FILE *outfile) {
-    /* TODO */
+    //iterate through wclist
+    //extract struct pointer from list element --> like in find_word
+    //fprintf to outfile like in word_count.c
+
+    for (struct list_elem *current = list_begin(wclist); current != list_end(wclist); current = list_next(current)) {
+        word_count_t *curStruct = list_entry(current, word_count_t, elem);
+        fprintf(outfile, "%8d\t%s\n", wc->count, wc->word);
+    }
 }
 
 static bool less_list(const struct list_elem *ewc1,
                       const struct list_elem *ewc2, void *aux) {
-    /* TODO */
-    return false;
+    //
+
+    bool (*less)(const word_count_t *, const word_count_t *) = aux;
+    word_count_t *wc1 = list_entry(ewc1, word_count_t, elem);
+    word_count_t *wc2 = list_entry(ewc2, word_count_t, elem);
+    return less(wc1, wc2);
 }
 
 void wordcount_sort(word_count_list_t *wclist,
